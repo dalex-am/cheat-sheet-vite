c5c144ff624fc2123c0866aa491792ec7eb5115d
Alexander Dolganov <a.dolganov@infomaximum.com>
2025-05-25 18:02:11 +0300

    feat: убрано всплывающее окно со страницы git-тренажёра

--- src/containers/Git/Git.tsx
+++ src/containers/Git/Git.tsx
@@ -5,7 +5,7 @@ import { IGitProps } from "./Git.types";
 export const Git: FC<IGitProps> = () => {
   return (
     <iframe
-      src="https://learngitbranching.js.org/?locale=ru_RU"
+      src="https://learngitbranching.js.org/?locale=ru_RU&NODEMO"
       title="iframe"
       sandbox="allow-same-origin allow-scripts"
       css={iframeStyle}
